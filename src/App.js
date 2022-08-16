3288991a20b06baf1abdf7e11176c146fb3ff399
DIVYANSH856 <email.divyansh.kohli@gmail.com>
2022-08-17 05:01:31 +0530

    ADDING BASIC FILES

---
+++ src/App.js
@@ -0,0 +1,40 @@
+
+import CategoryItem from './components/category-items/category-item';
+
+
+import './categories.styles.scss';
+
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    },
+  ];
+
+  return (
+    <div className='categories-container'>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
