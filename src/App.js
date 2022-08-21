20bce9530f0cbe9e47a0844e65f3f9a40949289b
DIVYANSH856 <email.divyansh.kohli@gmail.com>
2022-08-21 22:43:33 +0530

    added routes , added navbar and scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
+import './routes/navigation/navigation.styles.scss'
 
 import Home from './routes/home/home.component';
+
+const Shop =()=>{
+  return <h1>i am the shop page</h1>
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' index element={<Home />} />
+      <Route path='/' element={<Navigation/>}>
+        <Route  index element={<Home />} />
+        <Route path='Shop' element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
